baf8a510f6f6d6ed8ef96e78f8e582f9e477b396
luke <luke@lukes-awesome-freakinge-computer>
2026-08-09 20:08:07 -0400

    Save pointer now actually works

--- LKS-Cheat-Tool/LKS-Cheat-Tool/__init__.py
+++ LKS-Cheat-Tool/LKS-Cheat-Tool/__init__.py
@@ -60,7 +60,7 @@ def get_save_pos(location):
     else:
         save_pos_ptr = 0x80555ABC   
     curr_save_pos = dolphin_memory_engine.read_word(save_pos_ptr)
-    curr_save_pos = 0x903E8900
+    #curr_save_pos = 0x903E8900
     
     return location - init_save_pos + curr_save_pos
 
